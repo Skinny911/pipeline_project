a2be552538c8db5ab7297747052846a0c8a4693f
Skander Missaoui <93555412+Skinny911@users.noreply.github.com>
2025-01-02 18:33:32 +0100

    Update my_first_calculator.py

--- my_first_calculator.py
+++ my_first_calculator.py
@@ -1,18 +1,35 @@
 print('Welcome to this calculator!')
-print('It can add, subtract, multiply and divide whole numbers from 0 to 50')
-num1 = int(input('Please choose your first number: '))
-sign = input('What do you want to do? +, -, /, or *: ')
-num2 = int(input('Please choose your second number: '))
+print('Add, subtract, multiply and divide numbers')
+
+num1 = int(input('First number: '))
+sign = input('Sign? +, -, /, or *: ')
+num2 = int(input('Second number: '))
+
+# Calculator functions
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 == 0:
+        return "Cannot divide by zero"
+    return num1 / num2
                              
+# Perform calculation
 if sign == '+':
-    print(num1 + num2)
-elif sign == '-': 
-    print(num1 - num2)
-elif sign == '*': 
-    print(num1 * num2)
-elif sign == '/': 
-    print(num1 * num2)
-else: 
-    print("Invalid operator. Please use +, -, or *.")
+    print(add(num1, num2))
+elif sign == '-':
+    print(subtract(num1, num2))
+elif sign == '*':
+    print(multiply(num1, num2))
+elif sign == '/':
+    print(divide(num1, num2))
+else:
+    print("Invalid operator. Please use +, -, /, or *.")
 
 print("Thanks for using this calculator, goodbye :)")
