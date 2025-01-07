ab358006c159fb0725e0b319afd87975fa8dc814
Skander Missaoui <93555412+Skinny911@users.noreply.github.com>
2025-01-07 16:19:38 +0100

    Update my_first_calculator.py

--- my_first_calculator.py
+++ my_first_calculator.py
@@ -1,10 +1,3 @@
-print('Welcome to this calculator!')
-print('Add, subtract, multiply and divide numbers')
-
-num1 = int(input('First number: '))
-sign = input('Sign? +, -, /, or *: ')
-num2 = int(input('Second number: '))
-
 # Calculator functions
 def add(num1, num2):
     return num1 + num2
@@ -19,17 +12,28 @@ def divide(num1, num2):
     if num2 == 0:
         return "Cannot divide by zero"
     return num1 / num2
-                             
-# Perform calculation
-if sign == '+':
-    print(add(num1, num2))
-elif sign == '-':
-    print(subtract(num1, num2))
-elif sign == '*':
-    print(multiply(num1, num2))
-elif sign == '/':
-    print(divide(num1, num2))
-else:
-    print("Invalid operator. Please use +, -, /, or *.")
 
-print("Thanks for using this calculator, goodbye :)")
+# Interactive calculator logic
+def main():
+    print('Welcome to this calculator!')
+    print('Add, subtract, multiply, and divide numbers')
+
+    # Collect user inputs
+    num1 = int(input('First number: '))
+    sign = input('Sign? +, -, /, or *: ')
+    num2 = int(input('Second number: '))
+
+    # Preform calculations
+    if sign == '+':
+        print(add(num1, num2))
+    elif sign == '-':
+        print(subtract(num1, num2))
+    elif sign == '*':
+        print(multiply(num1, num2))
+    elif sign == '/':
+        print(divide(num1, num2))
+    else:
+        print("Invalid operator. Please use +, -, /, or *.")
+
+if __name__ == "__main__":
+    main()
